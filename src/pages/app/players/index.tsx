0bd8d963c9eaed8cf22480c7ec6b91bfb985c18a
Guillaume Dieudonné <dieug_@MacBook-Pro-de-Guillaume.local>
2021-10-29 18:14:23 +0200

    fix eslint & prettier config OK

--- src/pages/app/players/index.tsx
+++ src/pages/app/players/index.tsx
@@ -1,105 +1,105 @@
-import PlayersList from '@/client/components/Management/Users/PlayersList';
-import { Grid } from '@mui/material';
-import { ethers } from 'ethers';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useCallback, useEffect, useState } from 'react';
-import { PREDICTION_CONTRACT_ABI } from 'src/client/abis/pancake-prediction-abi-v3';
-import Footer from 'src/client/components/Footer';
-import PageHeader from 'src/client/components/Management/Users/PageHeader';
-import PageTitleWrapper from 'src/client/components/PageTitleWrapper';
-import { useGetCurrentUserQuery } from 'src/client/graphql/getCurrentUser.generated';
-import MainLayout from 'src/client/layouts/MainLayout';
-import loadPlayers from 'src/client/thegraph/loadPlayers';
+import PlayersList from '@/client/components/Management/Users/PlayersList'
+import { Grid } from '@mui/material'
+import { ethers } from 'ethers'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useCallback, useEffect, useState } from 'react'
+import { PREDICTION_CONTRACT_ABI } from 'src/client/abis/pancake-prediction-abi-v3'
+import Footer from 'src/client/components/Footer'
+import PageHeader from 'src/client/components/Management/Users/PageHeader'
+import PageTitleWrapper from 'src/client/components/PageTitleWrapper'
+import { useGetCurrentUserQuery } from 'src/client/graphql/getCurrentUser.generated'
+import MainLayout from 'src/client/layouts/MainLayout'
+import loadPlayers from 'src/client/thegraph/loadPlayers'
 
-import type { ReactElement } from 'react';
-import type { User } from 'src/client/models/user';
+import type { ReactElement } from 'react'
+import type { User } from 'src/client/models/user'
 function ManagementUsers() {
-  const router = useRouter();
+  const router = useRouter()
 
-  const [fetching, setFetching] = useState<boolean>(true);
-  const [players, setPlayers] = useState<any[]>([]);
-  const [epoch, setEpoch] = useState<string>(null);
-  const [hasError, setHasError] = useState<boolean>(false);
+  const [fetching, setFetching] = useState<boolean>(true)
+  const [players, setPlayers] = useState<any[]>([])
+  const [epoch, setEpoch] = useState<string>(null)
+  const [hasError, setHasError] = useState<boolean>(false)
 
-  const [user, setUser] = useState<User | any>(null);
-  const [preditionContract, setPreditionContract] = useState<any>(null);
-  const [provider, setProvider] = useState<ethers.providers.Web3Provider>(null);
+  const [user, setUser] = useState<User | any>(null)
+  const [preditionContract, setPreditionContract] = useState<any>(null)
+  const [provider, setProvider] = useState<ethers.providers.Web3Provider>(null)
 
-  const [{ data }] = useGetCurrentUserQuery();
+  const [{ data }] = useGetCurrentUserQuery()
 
   const getPlayers = useCallback(
     async (preditionContract) => {
-      if (epoch) return;
+      if (epoch) return
       try {
         // const _epoch = epoch ? epoch : await preditionContract.currentEpoch()
 
-        const epoch = await preditionContract.currentEpoch();
-        setEpoch(epoch);
+        const epoch = await preditionContract.currentEpoch()
+        setEpoch(epoch)
 
-        const players = await loadPlayers({ epoch });
-        setPlayers(players);
-        setFetching(false);
+        const players = await loadPlayers({ epoch })
+        setPlayers(players)
+        setFetching(false)
       } catch (err) {
-        setHasError(true);
+        setHasError(true)
       }
     },
     [epoch]
-  );
+  )
 
   const refreshQuery = useCallback(
     async ({ orderBy }) => {
-      let _epoch = epoch;
+      let _epoch = epoch
       if (!_epoch) {
-        _epoch = await preditionContract.currentEpoch();
-        setEpoch(_epoch);
+        _epoch = await preditionContract.currentEpoch()
+        setEpoch(_epoch)
       }
 
-      setFetching(true);
-      setPlayers([]);
-      players.length = 0;
+      setFetching(true)
+      setPlayers([])
+      players.length = 0
 
       try {
-        const _players = await loadPlayers({ epoch: _epoch, orderBy });
-        setPlayers(_players);
-        setFetching(false);
+        const _players = await loadPlayers({ epoch: _epoch, orderBy })
+        setPlayers(_players)
+        setFetching(false)
       } catch (err) {
-        setHasError(true);
+        setHasError(true)
       }
     },
     [epoch, players, preditionContract]
-  );
+  )
 
   useEffect(() => {
-    if (!data) return;
+    if (!data) return
     if (!data?.currentUser) {
-      router.push('/app');
-      return;
+      router.push('/app')
+      return
     }
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    setProvider(provider);
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
 
     // TODO decode from server
     // const privateKey = crpyto.decrypt(data.currentUser.private)
-    const privateKey = data.currentUser.private;
+    const privateKey = data.currentUser.private
 
-    const signer = new ethers.Wallet(privateKey, provider);
+    const signer = new ethers.Wallet(privateKey, provider)
 
-    setUser(data.currentUser);
+    setUser(data.currentUser)
     const preditionContract = new ethers.Contract(
       process.env.NEXT_PUBLIC_PANCAKE_PREDICTION_CONTRACT_ADDRESS,
       PREDICTION_CONTRACT_ABI,
       signer
-    );
-    setPreditionContract(preditionContract);
+    )
+    setPreditionContract(preditionContract)
 
     try {
-      getPlayers(preditionContract);
+      getPlayers(preditionContract)
     } catch (err) {
-      setHasError(true);
+      setHasError(true)
     }
-  }, [data, getPlayers, router, provider]);
+  }, [data, getPlayers, router, provider])
 
   return (
     <>
@@ -117,11 +117,11 @@ function ManagementUsers() {
       </Grid>
       <Footer />
     </>
-  );
+  )
 }
 
-export default ManagementUsers;
+export default ManagementUsers
 
 ManagementUsers.getLayout = function getLayout(page: ReactElement) {
-  return <MainLayout>{page}</MainLayout>;
-};
+  return <MainLayout>{page}</MainLayout>
+}
